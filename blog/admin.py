a7b66d5f74e5197e173081cea1635a09483a25a4
dm1tro69 <dimolg22@gmail.com>
2020-03-10 23:01:34 +0200

    add html

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'created', 'status',)
+    search_fields = ('author', 'title', 'publish',)
+    prepopulated_fields = {"slug": ("title",)}
